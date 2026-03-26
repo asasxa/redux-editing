7254736edcb65805000a0387123451650f4bb7d1
asasxa <asasxa@users.noreply.github.com>
2026-03-26 11:16:50 +0300

    i

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build](https://github.com/asasxa/redux-editing/actions/workflows/deploy.yml/badge.svg)](https://github.com/asasxa/redux-editing/actions)
+
+Демо: [https://asasxa.github.io/redux-editing/](https://asasxa.github.io/redux-editing/)
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import store from './store';
+import './index.css';
+
+const container = document.getElementById('root');
+
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
